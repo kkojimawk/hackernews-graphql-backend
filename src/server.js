52c85da2210359ada604c79b547582161ace2d81
kkojimawk <kkojimawk@gmail.com>
2022-11-30 02:06:58 +0900

    mutation定義

--- src/server.js
+++ src/server.js
@@ -1,9 +1,29 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+//HackerNewsの1つ1つの投稿
+let links = [
+  {
+    id: "link-0",
+    description: "GraphQLチュートリアルをUdemyで学ぶ",
+    url: "https://www.udemy.com/graphql-bootcamp/",
+  },
+];
+
 //GraphQLスキーマの定義
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Mutation {
+    post(url: String!, description: String!): Link!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
@@ -11,6 +31,21 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     info: () => `HackerNewsクローンa`,
+    feed: () => links,
+  },
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length;
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      links.push(link);
+      return link;
+    },
   },
 };
 
